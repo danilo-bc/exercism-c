4989a34e0bd18c3b92968dc836297af1e94ee308
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-04-02 09:03:15 -0300

    Slight code refactoring based on mentor feedback

--- armstrong-numbers/src/armstrong_numbers.c
+++ armstrong-numbers/src/armstrong_numbers.c
@@ -2,29 +2,20 @@
 #include <math.h>
 
 bool is_armstrong_number(int candidate){
-    int inp_copy = candidate;
-
     // Find out number of digits in number
     int num_digits = 0;
-    while(inp_copy > 0){
-        inp_copy = inp_copy/10;
+    for(int inp_copy = candidate; inp_copy > 0; inp_copy /= 10){
         num_digits = num_digits + 1;
     }
 
     // Extract individual digits
     // Accumulate digit^num_digits
     int sum_result = 0;
-    inp_copy = candidate;
-    for(int i = 0; i < num_digits; i++){
-        int digit;
-        digit = inp_copy % 10;
-        sum_result = sum_result + (int)pow(digit, num_digits);
-        inp_copy = inp_copy / 10;
+    for(int i = 0, inp_copy = candidate; i < num_digits; i++, inp_copy /= 10){
+        int digit = inp_copy % 10;
+        sum_result = sum_result + pow(digit, num_digits);
     }
 
-    if(sum_result == candidate)
-        return true;
-    else
-        return false;
+    return sum_result == candidate;
 
 }
