53ad59e996318331a0a2528003d05e2fee1a32e7
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-04-03 23:37:06 -0300

    Initial solution for resistor-color problem

--- resistor-color/src/resistor_color.c
+++ resistor-color/src/resistor_color.c
@@ -1,1 +1,13 @@
 #include "resistor_color.h"
+
+int color_code(resistor_band_t color){
+    return color;
+}
+
+resistor_band_t* colors(){
+    static resistor_band_t all_colors[] = {
+        BLACK, BROWN, RED, ORANGE, YELLOW,
+        GREEN, BLUE, VIOLET, GREY, WHITE
+    };
+    return all_colors;
+}
