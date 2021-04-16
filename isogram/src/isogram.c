dee205a7d2611793c1905193e30aa6fa083d159e
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-04-16 11:46:36 -0300

    Incremental improvement based on mentor feedback

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -4,28 +4,19 @@
 bool is_isogram(const char phrase[]){
     if(phrase == NULL) //NULL pointers return false
         return false;
-    else{
-        for(int i = 0; phrase[i] != '\0'; i++){ //Traverse string until null '\0' char
-            for(int j = 0; phrase[j] != '\0'; j++){
-                if(i != j){ // only test different chars
-                    if(isalpha(phrase[j])){ // check if is a letter
-                        if(phrase[i] == phrase[j]){
-                            return false;
-                        }
-                        else if(isupper(phrase[j])){ //if uppercase, try lowercase version
-                            if(phrase[i] == tolower(phrase[j]))
-                                return false;
-                        }
-                        else if(islower(phrase[j])){ // if lowercase, try uppercase version
-                            if(phrase[i] == toupper(phrase[j])){
-                                return false;
-                            }
-                        }
+
+    for(int i = 0; phrase[i] != '\0'; i++){ //Traverse string until null '\0' char
+        for(int j = 0; phrase[j] != '\0'; j++){
+            if(i != j){ // only test different chars
+                if(isalpha((unsigned char) phrase[j])){ // check if is a letter
+                    if(phrase[i] == tolower((unsigned char) phrase[j]) || phrase[i] == toupper((unsigned char) phrase[j])){
+                        return false;
                     }
-                }  
-            }  
+                }
+            }
         }
-        // If traversed all letters, it's an isogram!
-        return true;
     }
+     
+    // If traversed all letters, it's an isogram!
+    return true;
 }
