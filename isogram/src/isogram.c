9a440e0af6a11cb0c8d70e35d9ad5ad6d412f9cd
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-04-16 15:50:14 -0300

    Incremental improvements. Comment cleanup

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -4,23 +4,15 @@
 bool is_isogram(const char phrase[]){
     if(phrase == NULL) //NULL pointers return false
         return false;
-        
     // Supposing all letters are in the English alphabet
     bool letter_present[26] = {false};
-    for(size_t i = 0; phrase[i] != '\0'; i++){ //Traverse string until null '\0' char
-        if(isalpha((unsigned char) phrase[i])){ // Check if it's a letter
-            // use letter_present to check if it was already found
-            // if true, return false (not isogram)
-            // if false, turn into true and carry the search
-            if(letter_present[(unsigned char) tolower(phrase[i]) - 'a']){
+    for(size_t i = 0; phrase[i] != '\0'; i++){
+        if(isalpha((unsigned char) phrase[i])){ 
+            if(letter_present[tolower((unsigned char) phrase[i]) - 'a'])
                 return false;
-            }
-            else{
-                letter_present[(unsigned char) tolower(phrase[i]) - 'a'] = true;
-            }
+            else
+                letter_present[tolower((unsigned char) phrase[i]) - 'a'] = true;
         }
     }
-     
-    // If traversed all letters, it's an isogram!
-    return true;
+    return true; // If traversed all letters, it's an isogram!
 }
