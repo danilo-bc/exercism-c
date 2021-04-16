f172be60e13a2218ce070abf5bfaac28fb09fff8
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-04-16 13:50:20 -0300

    Less unnecessary looping

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -5,13 +5,11 @@ bool is_isogram(const char phrase[]){
     if(phrase == NULL) //NULL pointers return false
         return false;
 
-    for(int i = 0; phrase[i] != '\0'; i++){ //Traverse string until null '\0' char
-        for(int j = 0; phrase[j] != '\0'; j++){
-            if(i != j){ // only test different chars
-                if(isalpha((unsigned char) phrase[j])){ // check if is a letter
-                    if(phrase[i] == tolower((unsigned char) phrase[j]) || phrase[i] == toupper((unsigned char) phrase[j])){
-                        return false;
-                    }
+    for(size_t i = 0; phrase[i] != '\0'; i++){ //Traverse string until null '\0' char
+        for(size_t j = i+1; phrase[j] != '\0'; j++){
+            if(isalpha((unsigned char) phrase[j])){ // check if is a letter
+                if(phrase[i] == tolower((unsigned char) phrase[j]) || phrase[i] == toupper((unsigned char) phrase[j])){
+                    return false;
                 }
             }
         }
